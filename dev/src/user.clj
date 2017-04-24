213ed15665af26e85cd1a408d520f989b13db0fc
Dexter Gramfors <dexter.gramfors@gmail.com>
2017-04-25 00:24:36 +0200

    Refer integrant.repl.state constants.

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -2,7 +2,8 @@
   (:require [io.aviso.repl :as pretty]
             [integrant-app.core]
             [integrant-app.config.main-config :refer [main-config]]
-            [integrant.repl :refer [clear go halt init reset reset-all set-prep!]]))
+            [integrant.repl :refer [clear go halt init reset reset-all set-prep!]]
+            [integrant.repl.state :refer [config system]]))
 
 (pretty/install-pretty-exceptions)
 (integrant.repl/set-prep! (constantly main-config))
